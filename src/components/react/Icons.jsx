adea1cff0a8aca1e01bf6e38121f331714243be3
Adrian R. <161341529+adrirf7@users.noreply.github.com>
2026-01-12 12:05:19 +0100

    Adjust MarketlyLogo width for improved scaling and responsiveness

--- src/components/react/Icons.jsx
+++ src/components/react/Icons.jsx
@@ -120,7 +120,7 @@ export function MarketlyLogo() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="240"
+      width="150"
       height="48"
       viewBox="0 0 120 40"
     >
